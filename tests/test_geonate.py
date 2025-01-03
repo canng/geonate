bf2c5a65f8398a6bbee29fca97e9027e48c31dc4
Can Ng <61179051+canng@users.noreply.github.com>
2025-01-03 14:12:21 +0100

    Remove unnecessary test case

--- tests/test_geonate.py
+++ tests/test_geonate.py
@@ -2,20 +2,3 @@
 
 """Tests for `geonate` package."""
 
-
-import unittest
-
-from geonate import geonate
-
-
-class TestGeonate(unittest.TestCase):
-    """Tests for `geonate` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
