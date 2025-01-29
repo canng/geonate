4cfaa41e4cf25b42346f986164655c5f94480ff4
Can Ng <61179051+canng@users.noreply.github.com>
2025-01-29 23:17:13 +0100

    init commit

--- geonate/__init__.py
+++ geonate/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for geonate."""
 
-__author__ = """Can Nguyen"""
+__author__ = """Can Nguyen""" 
 __email__ = "ntcan.lres@gmail.com"
 __version__ = "0.0.1"
+
+
+# =================================================================================================================== #
+
+from .geonate import *
--- geonate/geonate.py
+++ geonate/geonate.py
@@ -1,1 +1,5 @@
-"""Main module."""
+"""
+Main module.
+
+"""
+
